dcf12f676d7d2cee090b69463d3cd8ada6f2c9e2
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2025-03-06 08:54:33 +0500

    sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://www.faceshapedetect.com',
+    generateRobotsTxt: true, 
+    changefreq: 'daily', 
+    priority: 0.7, 
+    sitemapSize: 5000, 
+  };
+  
